a566b2cfd0d1cc987c67460e8ca2a085c99fed27
Ankitsonu <47600539+AnkitSharmaSonu@users.noreply.github.com>
2022-04-19 16:24:07 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,10 @@
 # from subprocess import call
-
 # call(["pytest ", "--alluredir test1 ./SUITES"])
-
 import os 
 # os.system('pytest --alluredir test2 ./SUITES')
-
-
 import datetime
 basename = "mylogfile" +"_"
-basename_allure = "allure-results"
 suffix = datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S")
 filename = "".join([basename, suffix])
-
-print(f'pytest --alluredir {basename_allure} ./SUITES')
-
-os.system(f'pytest --alluredir {basename_allure} ./SUITES')
+print(f'pytest --alluredir {filename} ./SUITES')
+os.system(f'pytest --alluredir {filename} ./SUITES')

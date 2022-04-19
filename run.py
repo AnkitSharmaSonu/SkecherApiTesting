4fec592cd47fae0e35ce0322f53f5d90ca0ad049
AnkitSharmaSonu <sssonusharma1999@gmail.com>
2022-04-19 16:31:35 +0530

    --->>> added

--- run.py
+++ run.py
@@ -3,8 +3,8 @@
 import os 
 # os.system('pytest --alluredir test2 ./SUITES')
 import datetime
-basename = "mylogfile" +"_"
+basename = "allure-results" 
 suffix = datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S")
 filename = "".join([basename, suffix])
 print(f'pytest --alluredir {filename} ./SUITES')
-os.system(f'pytest --alluredir {filename} ./SUITES')
+os.system(f'pytest --alluredir {basename} ./SUITES')
